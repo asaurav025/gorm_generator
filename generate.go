61bcbf69779da4e594ab9022878a1a66e3d6a8db
sauravkumar@zestmoney.in <asaurav025@gmail.com>
2019-09-21 01:24:13 +0530

    added some error handling

---
+++ generate.go
@@ -0,0 +1,39 @@
+package gormgenerator
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Generate generate model files according to their name
+func Generate(path string) {
+	modelList := []Model{}
+	yamlFile, err := ioutil.ReadFile("test.yaml")
+	if err != nil {
+		panic(err)
+	}
+	yaml.Unmarshal([]byte(yamlFile), &modelList)
+	modelTemp, err := ioutil.ReadFile("modelGenerator/model.go.tmpl")
+	if err != nil {
+		panic(err)
+	}
+	tmpl, err := template.New("model").Parse(string(modelTemp))
+	if err != nil {
+		panic(err)
+	}
+
+	for _, modelT := range modelList {
+		modelFilePath := fmt.Sprintf("%s/%s.go", path, modelT.Name)
+		f, err := os.Create(modelFilePath)
+		if err != nil {
+			panic(err)
+		}
+		tmpl.Execute(f, &modelT)
+		f.Close()
+	}
+
+}
